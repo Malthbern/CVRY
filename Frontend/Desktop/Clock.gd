0b29c157244eb191dadcb02552d83af6027ed60f
Malthbern <grantjackson95@gmail.com>
2024-05-10 02:44:41 -0500

    Started work on creating desktop UI

---
+++ Frontend/Desktop/Clock.gd
@@ -0,0 +1,13 @@
+extends LineEdit
+
+var datetime
+var time
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	datetime = Time.get_datetime_dict_from_system(false)
+	if datetime.minute < 10:
+		time = '%s:0%s' % [datetime.hour, datetime.minute]
+	else: 
+		time = '%s:%s' % [datetime.hour, datetime.minute]
+	text = time
